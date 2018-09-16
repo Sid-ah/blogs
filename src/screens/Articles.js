81f2166ff2e17accb3d657b26080468e3fa1faa8
sid-ah <sidah_10@hotmail.com>
2018-09-16 12:50:19 -0400

    Render the articles with FlatList

--- src/screens/Articles.js
+++ src/screens/Articles.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Dimensions,
-  TouchableHighlights,
-} from 'react-native';
+import {View, Text, StyleSheet, Dimensions, FlatList} from 'react-native';
 import {connect} from 'react-redux';
-const {width} = Dimensions.get ('window');
+const {width, height} = Dimensions.get ('window');
 
 class Articles extends React.Component {
   static navigationOptions = {
     title: 'News Feed',
   };
-  render () {
+  renderItem = article => {
     return (
-      <View>
-        <Text>Hello World</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>{article.item.Author}</Text>
+        <Text style={styles.instructions}>
+          {article.item.description ||
+            'JAck sparrow like to dance with the stars'}
+        </Text>
+        <Text style={styles.instructions}>{article.item.city}</Text>
       </View>
+    );
+  };
+  _keyExtractor = article => {
+    return article.Author;
+  };
+  render () {
+    const {articles} = this.props;
+    return (
+      <FlatList
+        data={articles}
+        renderItem={this.renderItem}
+        keyExtractor={this._keyExtractor}
+        style={styles.listContainer}
+      />
     );
   }
 }
@@ -29,3 +42,26 @@ const mapStateToProps = state => {
 };
 
 export default connect (mapStateToProps, {}) (Articles);
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  listContainer: {
+    height,
+    width,
+  },
+});
